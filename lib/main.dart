77fd142bccbbf2597c0444adcc2567c5c8b1820b
Raajeev Chandran <raajeevchandran787@gmail.com>
2021-04-23 22:10:55 +0530

    homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,26 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:platform_action_sheet/platform_action_sheet.dart';
 import 'package:skinmed/homePage.dart';
 import 'package:tflite/tflite.dart';
 
 void main() {
-  runApp(MaterialApp(home: MyApp()));
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(MaterialApp(
+    theme:ThemeData(
+      fontFamily: GoogleFonts.philosopher().fontFamily
+    ),
+      builder: (context, child) {
+        return ScrollConfiguration(
+          behavior: CustomScrollBehavior(),
+          child: child,
+        );
+      },
+      home: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -91,5 +104,13 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: HomeScreen());
+  }
+}
+
+class CustomScrollBehavior extends ScrollBehavior {
+  @override
+  Widget buildViewportChrome(
+      BuildContext context, Widget child, AxisDirection axisDirection) {
+    return child;
   }
 }
